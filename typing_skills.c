99c6b45f75e554ba3020ae2408845eea59d7edda
Rajeev Negi <debugyourlife@outlook.com>
2024-12-01 16:53:29 +0530

    Update typing_skills.c

--- typing_skills.c
+++ typing_skills.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h> // using unistd for linux 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <functions.c>
 
-
-void timer_start(time_t *start  ){
-    // start the timer
-    time(*&start);
-    printf("Timer started...\n"); 
-}
-
-void timer_end(time_t *end  ){
-    // end the timer
-    time(*&end);
-    printf("Timer stopped.\n"); 
-}
 
 
 
@@ -22,23 +13,41 @@ int main() {
     time_t start_time, end_time;
     double elapsed_time;
     char words[500];
+    int cmd;
+    bool exit = false;
 
-    //time started
-    timer_start(&start_time);
-    
+    // Game Menu Loop
+    do{
+    printf("welcome to typing test v1.0\n\n1 - start\n2 - show leaderboard\n3 - Quit\ninput:");
+    scanf("%d",&cmd);
 
-    printf("enter words:");
-    fgets(words,sizeof(words),stdin);
+    switch(cmd){
+    case 1:
+        Game(start_time,&exit);
+        break;
+    case 2:
+        Leaderboard();
+        system("clear");
+        break;
+    case 3:
+        exit = true;
+        break;
+    }
+    }while(!exit);
     
-    //time ended
-    timer_end(&end_time);
+    
+    
+    /* printf("enter words:");
+    fgets(words,sizeof(words),stdin); */
+    
+    
 
     // Calculate the elapsed time
     elapsed_time = difftime(end_time, start_time);
     
 
     // Display the elapsed time
-    printf("Elapsed time: %.2f seconds\n", elapsed_time);
+    //printf("Elapsed time: %.2f seconds\n", elapsed_time);
 
     return 0;
 }
